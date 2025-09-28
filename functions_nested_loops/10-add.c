c4c59ec6f0f19c65af3a46d3a0e898f473316ce7
desparvis <latifahm501@gmail.com>
2025-09-28 14:38:09 -0700

    add code 10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function to add and return result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: return the sum of int a and int b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
