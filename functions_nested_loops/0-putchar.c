36754090e4c19cdf92b904cfb3223a7097165f07
desparvis <latifahm501@gmail.com>
2025-09-28 12:25:53 -0700

    putchar print

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point of the program
+ * Description - This prints _putchar followed by a new line
+ * Return: 0 to exit
+ */
+int main(void) 
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project is about nested loops, functions, and how to use them. It is a learning project.
